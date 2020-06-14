733dc132ee20f8762d31be35acbaa4e0202d79b6
David Bycroft <55767231+bycroft-d@users.noreply.github.com>
2020-06-14 19:13:29 -0300

    Update and rename Index.html to hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+x = 5
+y = "Great"
+a, d, u = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+# Assigning Values to Multiple Variables
+print(a)
+print(d)
+print(u)
+# Output Variables
+print("Python is " + y)
+# print("Python is " + x)
